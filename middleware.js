aa95811cb191e4f174cec2e94dd497e54bbb50c1
AbdullahAli2005 <smabd7409@gmail.com>
2026-02-07 15:00:56 +0500

    prisma config

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
